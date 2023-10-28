fcf940fda80c1c25ccfbf749ccb857cf28631152
MikeJhoeB <mikejbsouza@gmail.com>
2023-10-27 23:26:19 -0300

    feat: added RestTemplate Bean to do an external request

---
+++ paymentProject/src/main/java/com/paymentProject/configurations/ProjectConfiguration.java
@@ -0,0 +1,15 @@
+package com.paymentProject.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ProjectConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
